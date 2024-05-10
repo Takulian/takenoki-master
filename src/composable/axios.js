62d62c8dc72819de94e326e750d02dbaca898966
Takulian <miftakhulghulam10@gmail.com>
2024-05-10 20:03:57 +0700

    add api and another needs

---
+++ src/composable/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { useCookies } from "@vueuse/integrations/useCookies";
+
+const useAxios = (path = "") => {
+  // const cookies = useCookies();
+  // const token = cookies.get("session-admin");
+
+  const axiosClient = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL + path,
+    // headers: {
+    //   Authorization: `Bearer ${token}`,
+    // },
+  });
+
+  return axiosClient;
+};
+
+export default useAxios;
