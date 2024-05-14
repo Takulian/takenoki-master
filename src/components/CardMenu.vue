f4676f51a3a4a6f0d3579a3cb32a6898628c1607
Takulian <miftakhulghulam10@gmail.com>
2024-05-14 22:10:26 +0700

    Change display menu system

---
+++ src/components/CardMenu.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { onMounted } from "vue";
+import Makanan from "@/data/dmenu.json";
+import useMenu from "@/composable/menu";
+
+const { menus, getMenuc } = useMenu();
+const menu = defineProps(["type"]);
+
+onMounted(() => {
+  getMenuc(menu.type);
+});
+</script>
+
+<template>
+  <div
+    class="pl-[10vh] tulisan text-5xl pt-10 pb-5 bg-[var(--color-sand)] flex items-center text-nowrap"
+    style="color: var(--color-hitam)"
+  >
+    <div>{{ menu.type }}</div>
+    <div class="w-full rounded-lg bg-[var(--color-merah)] h-3 ml-10 mr-3"></div>
+  </div>
+  <!-- <div class="judul tulisan bg text-5xl pt-10 pb-5">{{ menu.type }}</div> -->
+  <div class="bg flex flex-wrap justify-center">
+    <el-card
+      shadow="never"
+      class="el-card h-full mx-2 mt-4 inline-block"
+      v-for="item in menus"
+      :key="item"
+    >
+      <div class="flex justify-center">
+        <img
+          class="rounded-md min-h-52 max-h-52 object-cover min-w-52 max-w-52"
+          :src="item.gambar"
+          alt="Gambar Menu"
+        />
+      </div>
+      <p class="text-center tulisan pt-4">{{ item.nama }}</p>
+      <p class="text-center tulisan-kecil">Harga: Rp {{ item.harga }}</p>
+      <template #footer>
+        <p class="text-center tulisan">Deskripsi</p>
+        <p class="tulisan-kecil text-base whitespace-normal text-center">
+          {{ item.short_desc }}
+        </p>
+      </template>
+    </el-card>
+  </div>
+  <div class="bg pb-10"></div>
+</template>
+
+<style scoped>
+.el-card {
+  width: 17vw;
+  min-height: 30vw;
+  background-color: var(--color-persian);
+}
+
+.tulisan-kecil {
+  font-style: "poppins", sans-serif;
+  font-weight: 300;
+  color: white;
+  font-size: 14px;
+}
+
+.tulisan {
+  font-style: "poppins", sans-serif;
+  font-weight: 700;
+  color: white;
+}
+
+::-webkit-scrollbar {
+  height: 5px !important;
+}
+
+::-webkit-scrollbar-thumb {
+  background-image: var(--color-hitam);
+  border-radius: 10px;
+}
+
+.bg {
+  background-color: var(--color-sand);
+}
+</style>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,35 +1,99 @@
-<script setup>
+<script lang="ts" setup>
+import { ref } from "vue";
 import NavBar from "@/components/NavBar.vue";
 import Menu from "@/components/Menu.vue";
-import Ramen from "@/components/DisplayMenu/Ramen.vue";
-import Udon from "@/components/DisplayMenu/Udon.vue";
-import Donburi from "@/components/DisplayMenu/Donburi.vue";
-import AlaCarte from "@/components/DisplayMenu/AlaCarte.vue";
-import SpecialRamen from "@/components/DisplayMenu/SpecialRamen.vue";
-import SpecialUdon from "@/components/DisplayMenu/SpecialUdon.vue";
-import SpecialAgemono from "@/components/DisplayMenu/SpecialAgemono.vue";
-import Apetizer from "@/components/DisplayMenu/Apetizer.vue";
-import Topping from "@/components/DisplayMenu/Topping.vue";
-import Gohan from "@/components/DisplayMenu/Gohan.vue";
-import Minuman from "@/components/DisplayMenu/Minuman.vue";
+import CardMenu from "@/components/CardMenu.vue";
 import Footer from "@/components/Footer.vue";
+
+const category = ref<
+  | "AlaCarte"
+  | "Apetizer"
+  | "Donburi"
+  | "Gohan"
+  | "Minuman"
+  | "Ramen"
+  | "Special Agemono"
+  | "Special Donburi"
+  | "Special Ramen"
+  | "Special Udon"
+  | "Topping"
+  | "Udon"
+>("Ramen");
 </script>
 
 <template>
   <main>
     <NavBar />
     <Menu />
-    <Ramen />
-    <Udon />
-    <Donburi />
-    <AlaCarte />
-    <SpecialRamen />
-    <SpecialUdon />
-    <SpecialAgemono />
-    <Apetizer />
-    <Topping />
-    <Gohan />
-    <Minuman />
+    <div class="bg-[var(--color-sand)] pt-10">
+      <div
+        class="flex justify-center font-bold text-[var(--color-hitam)] text-4xl"
+      >
+        Click to see menu
+      </div>
+      <div class="flex justify-center mt-6">
+        <el-radio-group
+          id="menuRadio"
+          fill="var(--color-persian)"
+          v-model="category"
+          aria-label="category control"
+          size="large"
+        >
+          <el-radio-button value="AlaCarte">AlaCarte</el-radio-button>
+          <el-radio-button value="Apetizer">Apetizer</el-radio-button>
+          <el-radio-button value="Donburi">Donburi</el-radio-button>
+          <el-radio-button value="Gohan">Gohan</el-radio-button>
+          <el-radio-button value="Minuman">Minuman</el-radio-button>
+          <el-radio-button value="Ramen">Ramen</el-radio-button>
+          <el-radio-button value="Special Agemono"
+            >Special Agemono</el-radio-button
+          >
+          <el-radio-button value="Special Donburi"
+            >Special Donburi</el-radio-button
+          >
+          <el-radio-button value="Special Ramen">Special Ramen</el-radio-button>
+          <el-radio-button value="Special Udon">Special Udon</el-radio-button>
+          <el-radio-button value="Topping">Topping</el-radio-button>
+          <el-radio-button value="Udon">Udon</el-radio-button>
+        </el-radio-group>
+      </div>
+    </div>
+    <div v-if="category === 'Ramen'">
+      <CardMenu type="Ramen" />
+    </div>
+    <div v-else-if="category === 'Udon'">
+      <CardMenu type="Udon" />
+    </div>
+    <div v-else-if="category === 'AlaCarte'">
+      <CardMenu type="AlaCarte" />
+    </div>
+    <div v-else-if="category === 'Apetizer'">
+      <CardMenu type="Apetizer" />
+    </div>
+    <div v-else-if="category === 'Donburi'">
+      <CardMenu type="Donburi" />
+    </div>
+    <div v-else-if="category === 'Gohan'">
+      <CardMenu type="Gohan" />
+    </div>
+    <div v-else-if="category === 'Minuman'">
+      <CardMenu type="Minuman" />
+    </div>
+    <div v-else-if="category === 'Special Agemono'">
+      <CardMenu type="Special Agemono" />
+    </div>
+    <div v-else-if="category === 'Special Donburi'">
+      <CardMenu type="Special Donburi" />
+    </div>
+    <div v-else-if="category === 'Special Ramen'">
+      <CardMenu type="Special Ramen" />
+    </div>
+    <div v-else-if="category === 'Special Udon'">
+      <CardMenu type="Special Udon" />
+    </div>
+    <div v-else-if="category === 'Topping'">
+      <CardMenu type="Topping" />
+    </div>
     <Footer />
   </main>
 </template>
